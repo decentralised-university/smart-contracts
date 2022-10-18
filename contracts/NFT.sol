202184cf1521920406ccfd411239785617d8583e
cameronmcewan <cm17521@bristol.ac.uk>
2022-10-18 19:16:41 +0100

    NFT Access Pass contract - enable anyone to mint

--- contracts/NFT.sol
+++ contracts/NFT.sol
@@ -2,28 +2,45 @@
 pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract AccessPass is ERC721, ERC721Enumerable, Ownable {
-    using Counters for Counters.Counter;
+contract AccessPass is ERC721, Ownable {
+    uint256 public mintPrice = 0.05 ether;  
+    uint256 public totalSupply;
+    uint256 public maxSupply;
+    bool public isMintEnabled;
+    mapping (address => uint) public mintedWallets;
 
-    Counters.Counter private _tokenIdCounter;
+    constructor() ERC721("AccessPass", "DUP") {
+        maxSupply = 20;
+    }
 
-    constructor() ERC721("AccessPass", "DUP") {}
+    function toggleMintEnabled() external onlyOwner {
+        isMintEnabled = !isMintEnabled;
+    }
 
-    function safeMint(address to) public onlyOwner {
-        uint256 tokenId = _tokenIdCounter.current();
-        _tokenIdCounter.increment();
-        _safeMint(to, tokenId);
+    function setMaxSupply(uint256 maxSupply_) external onlyOwner {
+        maxSupply = maxSupply_;
+    } 
+
+    function mint() external payable {
+        require(isMintEnabled, 'Minting is not currently enabled');
+        require(mintedWallets[msg.sender] < 1, 'You already own 1 Access Pass! You cannot buy more than 1.');
+        require(msg.value == mintPrice, 'Wrong value.');
+        require(maxSupply > totalSupply, 'Sold out!');
+
+        mintedWallets[msg.sender]++;
+        totalSupply++;
+        uint256 tokenId = totalSupply;
+        _safeMint(msg.sender, tokenId);
     }
 
     // The following functions are overrides required by Solidity.
 
     function _beforeTokenTransfer(address from, address to, uint256 tokenId)
         internal
-        override(ERC721, ERC721Enumerable)
+        override(ERC721)
     {
         super._beforeTokenTransfer(from, to, tokenId);
     }
@@ -31,9 +48,9 @@ contract AccessPass is ERC721, ERC721Enumerable, Ownable {
     function supportsInterface(bytes4 interfaceId)
         public
         view
-        override(ERC721, ERC721Enumerable)
+        override(ERC721)
         returns (bool)
-    {
+    {   
         return super.supportsInterface(interfaceId);
     }
 }
